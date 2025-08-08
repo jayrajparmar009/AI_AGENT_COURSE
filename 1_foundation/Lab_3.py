44f4cc592a6c0af081fac9d6bfdcd9f1fc02ea66
Jayraj Parmar <jayrajparmar009@gmail.com>
2025-08-07 22:25:03 -0400

    Finished Lab_3

--- 1_foundation/Lab_3.py
+++ 1_foundation/Lab_3.py
@@ -101,8 +101,48 @@ def evaluate(reply, message, history) -> Evaluation:
     response = gemini.beta.chat.completions.parse(model="gemini-2.0-flash", messages=messages, response_format=Evaluation)
     return response.choices[0].message.parsed
 
+
+# Testing out the chatbot with a sample question
 messages = [{"role": "system", "content": system_prompt}] + [{"role": "user", "content": "do you hold a patent?"}]
 response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
 reply = response.choices[0].message.content
 
+evaluate(reply, "Do you hold a patent?", messages[:1])
 
+
+
+# Now writting function for rerunning the query if the reply is not acceptable
+
+def rerun(reply, message, history, feedback):
+    updated_system_prompt = system_prompt + "\n\n## Previous answer rejected\nYou just tried to reply, but the quality control rejected your reply\n"
+    updated_system_prompt += f"## Your attempted answer:\n{reply}\n\n"
+    updated_system_prompt += f"## Reason for rejection:\n{feedback}\n\n"
+    messages = [{"role": "system", "content": updated_system_prompt}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    return response.choices[0].message.content
+
+
+
+# Creating a fun little function to test if the rerun function works, function below is telling LLM to respond in pig latin if the question
+# contains the word patent
+    def chat(message, history):
+        if "patent" in message:
+            system = system_prompt + "\n\nEverything in your reply needs to be in pig latin - \
+                it is mandatory that you respond only and entirely in pig latin"
+        else:
+            system = system_prompt
+        messages = [{"role": "system", "content": system}] + history + [{"role": "user", "content": message}]
+        response = openai.chat.completions.create(model="gpt-4o-mini", messages=messages)
+        reply =response.choices[0].message.content
+
+        evaluation = evaluate(reply, message, history)
+        
+        if evaluation.is_acceptable:
+            print("Passed evaluation - returning reply")
+        else:
+            print("Failed evaluation - retrying")
+            print(evaluation.feedback)
+            reply = rerun(reply, message, history, evaluation.feedback)       
+        return reply
+
+gr.ChatInterface(chat, type="messages").launch()
